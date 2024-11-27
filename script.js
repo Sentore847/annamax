a0c06efe6eb40bddfa2d6f2cb086d887bc2cba4a
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-27 17:20:22 +0200

    with modal

--- script.js
+++ script.js
@@ -1,28 +1,202 @@
-const customSelect = document.querySelector(".custom-select");
-const selectedOption = customSelect.querySelector(".selected-option");
-const options = customSelect.querySelectorAll(".options li");
+// const customSelect = document.querySelector(".custom-select");
+// const selectedOption = customSelect.querySelector(".selected-option");
+// const options = customSelect.querySelectorAll(".options li");
+const carSelect = document.querySelector("#car_make");
+const modelSelect = document.querySelector("#car_model");
+const carSelectedValue = carSelect.value;
+const cardButtons = document.querySelectorAll(".card_btn");
+const modalWindow = document.getElementById("modal_window");
+const closeModalBtn = document.querySelector(".modal_close_btn");
 
-customSelect.addEventListener("click", function () {
-  this.classList.toggle("open");
-});
+//Close Modal
+function closeModal() {
+  modalWindow.classList.remove("active");
+  closeModalBtn.classList.remove("active");
+}
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    customSelect.classList.remove("open");
-  }
-});
-
-document.addEventListener("click", function (event) {
-  if (!customSelect.contains(event.target)) {
-    customSelect.classList.remove("open");
-  }
-});
-
-options.forEach((option) => {
-  option.addEventListener("click", function () {
-    selectedOption.innerHTML = this.innerHTML;
+cardButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modalWindow.classList.add("active");
   });
-  customSelect.classList.remove("open");
+});
+
+modalWindow.addEventListener("click", (event) => {
+  if (event.target !== modalWindow) {
+    return;
+  }
+  closeModal();
+});
+
+closeModalBtn.addEventListener("click", () => {
+  closeModal();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+
+// Selects with Cars
+
+carSelect.addEventListener("change", () => {
+  const selectedMake = carSelect.value;
+  const modelOptions = [];
+
+  switch (selectedMake) {
+    case "toyota":
+      modelOptions.push(
+        "Corolla",
+        "Camry",
+        "RAV4",
+        "Land Cruiser",
+        "Hilux",
+        "Yaris",
+        "Supra",
+        "Prius",
+        "Avalon",
+        "Highlander",
+        "Tacoma",
+        "Sequoia"
+      );
+      break;
+    case "volkswagen":
+      modelOptions.push(
+        "Golf",
+        "Passat",
+        "Tiguan",
+        "Polo",
+        "Jetta",
+        "Arteon",
+        "ID.4",
+        "ID.3",
+        "Touareg",
+        "Beetle",
+        "Amarok"
+      );
+      break;
+    case "ford":
+      modelOptions.push(
+        "Focus",
+        "Mustang",
+        "Explorer",
+        "F-150",
+        "Escape",
+        "Fusion",
+        "Edge",
+        "Expedition",
+        "Bronco",
+        "Ranger",
+        "Maverick"
+      );
+      break;
+    case "honda":
+      modelOptions.push(
+        "Civic",
+        "Accord",
+        "CR-V",
+        "Pilot",
+        "Fit",
+        "Odyssey",
+        "Ridgeline",
+        "HR-V",
+        "Passport",
+        "Insight"
+      );
+      break;
+    case "bmw":
+      modelOptions.push(
+        "X5",
+        "3 Series",
+        "5 Series",
+        "7 Series",
+        "X3",
+        "X1",
+        "X7",
+        "M3",
+        "M5",
+        "i4",
+        "iX"
+      );
+      break;
+    case "mercedes":
+      modelOptions.push(
+        "C-Class",
+        "E-Class",
+        "GLE",
+        "S-Class",
+        "GLC",
+        "GLS",
+        "A-Class",
+        "CLA-Class",
+        "EQB",
+        "EQC",
+        "AMG GT"
+      );
+      break;
+    case "hyndai":
+      modelOptions.push(
+        "Elantra",
+        "Santa Fe",
+        "Tucson",
+        "Accent",
+        "Kona",
+        "Sonata",
+        "Palisade",
+        "Ioniq 5",
+        "Kona Electric",
+        "Veloster",
+        "Nexo"
+      );
+      break;
+    case "nissan":
+      modelOptions.push(
+        "Altima",
+        "Rogue",
+        "Murano",
+        "Pathfinder",
+        "Sentra",
+        "Maxima",
+        "Armada",
+        "Juke",
+        "Titan",
+        "Leaf"
+      );
+      break;
+    case "tesla":
+      modelOptions.push(
+        "Model S",
+        "Model 3",
+        "Model X",
+        "Model Y",
+        "Cybertruck"
+      );
+      break;
+    case "kia":
+      modelOptions.push(
+        "Sportage",
+        "Sorento",
+        "Rio",
+        "Optima",
+        "Soul",
+        "Telluride",
+        "Stinger",
+        "Forte",
+        "Carnival",
+        "Niro"
+      );
+      break;
+    default:
+      modelOptions.push("Please select a car make");
+  }
+  modelSelect.innerHTML = "";
+
+  modelOptions.forEach((model) => {
+    const option = document.createElement("option");
+    option.value = model;
+    option.text = model;
+    modelSelect.appendChild(option);
+  });
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -31,15 +205,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const runCounter = (counter) => {
     const target = +counter.getAttribute("data-count");
-    const count = +counter.innerText;
+    let count = +counter.innerText;
     const increment = target / speed;
 
-    if (count < target) {
-      counter.innerText = Math.ceil(count + increment);
-      setTimeout(() => runCounter(counter), 10);
-    } else {
-      counter.innerText = target;
-    }
+    const interval = setInterval(() => {
+      if (count < target) {
+        count += increment;
+        counter.innerText = Math.min(Math.ceil(count), target);
+      } else {
+        counter.innerText = target;
+        clearInterval(interval);
+      }
+    }, 10);
   };
 
   const observer = new IntersectionObserver(
@@ -57,3 +234,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
   counters.forEach((counter) => observer.observe(counter));
 });
+
+// Close lang switcher
+
+// customSelect.addEventListener("click", function () {
+//   this.classList.toggle("open");
+// });
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "Escape") {
+//     customSelect.classList.remove("open");
+//   }
+// });
+
+// document.addEventListener("click", function (event) {
+//   if (!customSelect.contains(event.target)) {
+//     customSelect.classList.remove("open");
+//   }
+// });
+
+// options.forEach((option) => {
+//   option.addEventListener("click", function () {
+//     selectedOption.innerHTML = this.innerHTML;
+//   });
+//   customSelect.classList.remove("open");
+// });
+
+// Count figures
