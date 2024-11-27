d578ef95ac9ceaf08cf185ded43ee82358021170
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-27 12:37:23 +0200

    Full layout

--- script.js
+++ script.js
@@ -2,32 +2,58 @@ const customSelect = document.querySelector(".custom-select");
 const selectedOption = customSelect.querySelector(".selected-option");
 const options = customSelect.querySelectorAll(".options li");
 
-// Открытие/закрытие селекта при клике
 customSelect.addEventListener("click", function () {
   this.classList.toggle("open");
 });
 
-// Закрытие селекта при нажатии Esc
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     customSelect.classList.remove("open");
   }
 });
 
-// Закрытие селекта при клике вне элемента
 document.addEventListener("click", function (event) {
   if (!customSelect.contains(event.target)) {
     customSelect.classList.remove("open");
   }
 });
 
-// Обновление выбранного элемента
 options.forEach((option) => {
   option.addEventListener("click", function () {
-    // Устанавливаем текст и иконку выбранной опции
     selectedOption.innerHTML = this.innerHTML;
-
-    // Закрываем селект
   });
   customSelect.classList.remove("open");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter_item_number");
+  const speed = 200;
+
+  const runCounter = (counter) => {
+    const target = +counter.getAttribute("data-count");
+    const count = +counter.innerText;
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(() => runCounter(counter), 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const counter = entry.target;
+          runCounter(counter);
+          observer.unobserve(counter);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  counters.forEach((counter) => observer.observe(counter));
 });
